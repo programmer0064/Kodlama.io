7cae6028e46a1285c2c7248abeceb2f116f1cd54
Berk0064 <berk6405@hotmail.com>
2023-05-18 10:08:55 +0200

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,22 @@
+""" In object-oriented programming, everything is about objects and classes. From this point of view, data types are actually classes and variables are instances of these classes, so objects.
+There are different kind of data types. There are multiple data types: """
+
+#Numeric data
+age = 5 #int - whole numbers
+height = 180.5 #float - decimal numbers
+
+#Sequence data
+name = "Engin Demirog" #collection of more than one character
+list = ["Python", "Java", "Java Script", 5] #like arrays but they can carry more than one data type and are flexible since you can easily replace the variables within the list
+tuple = ("turkish", "english", 8) #only difference to a list is that tuples are not changeable
+
+
+#Boolean
+truth = True  #can only return True or False
+lie = False
+
+#Dictionary
+translator = {"yemek" : "to eat", "yatmak" : "to sleep"}   #stores key : value pairs, whereas each pair is seperated by a comma
+
+
+
