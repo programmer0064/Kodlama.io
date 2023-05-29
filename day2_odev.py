6d9562b2f1a0c3d86c0693b4822f627dfe5d15f9
Berk0064 <berk6405@hotmail.com>
2023-05-29 14:18:01 +0200

    Update day2_odev.py

--- day2_odev.py
+++ day2_odev.py
@@ -6,8 +6,7 @@ for i in students_class:
 print("\n") 
 
 def addStudent(students):
-    addstudent = input("Do you want to add a student? Type in yes or no:    ")
-    print("\n") 
+    addstudent = input("Do you want to add a student? Type in yes or no:    ") 
 
     if addstudent == "yes":
         studentname = input("Type in the student's name: ")
@@ -26,7 +25,6 @@ print("\n")
 
 def deleteStudent(students):
     deletestudent = input("Do you want to delete a student? Type in yes or no:    ")
-    print("\n") 
 
     if deletestudent == "yes":
         studentname = str(input("Type in the student's name: "))
@@ -43,7 +41,41 @@ def deleteStudent(students):
 
 print("\n")
 
+def addMoreThanOneStudent(students):
+    addstudent=input("Do you want to add a student? yes/no ")
+    if addstudent == "yes":     
+        numberofstudents = int(input("How many students do you want to add? "))
+        i=0
+        for i in range(numberofstudents):
+            name = input("What is the name of the student? ")
+            students.append(name)
+        print("Students are added. Now the class exists of the following students: ")
+        for i in students:
+            print(i)
+
+print("\n")
+
+def indexOfStudent(students):
+    name = input("I can give you the index number of each student in the list. Which student are you looking for? ")
+    print("Student with the name {name} has the index number: " + str(students.index(name)))
+
+print("\n")
+
+def deleteMoreThanOneStudent(students):
+    deletestudent=input("Do you want to delete a student? yes/no ")
+    if deletestudent == "yes":     
+        numberofstudents = int(input("How many students do you want to delete? "))
+        i=0
+        for i in range(numberofstudents):
+            name = input("What is the name of the student? ")
+            students.remove(name)
+        print("Students are deleted from list. Now the class exists of the following students: ")
+        for i in students:
+            print(i)
 
 
 addStudent(students_class)
 deleteStudent(students_class)
+addMoreThanOneStudent(students_class)
+indexOfStudent(students_class)
+deleteMoreThanOneStudent(students_class)
