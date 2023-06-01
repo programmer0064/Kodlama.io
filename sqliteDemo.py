d2245204df321ce93470dd134a9eca76831a5057
Berk0064 <berk6405@hotmail.com>
2023-06-01 13:08:21 +0200

    Database coding

--- sqliteDemo.py
+++ sqliteDemo.py
@@ -2,11 +2,16 @@ import sqlite3
 
 def list():
     connection = sqlite3.connect("chinook/chinook.db")
-    cursor = connection.execute("select CustomerId,FirstName,LastName from customers")
+    cursor = connection.execute("select CustomerId,FirstName,LastName,Company,Address,City,State,Country,PostalCode,Phone,Fax,Email,SupportRepId from customers")
 
     for line in cursor:
-        print(line[2])
+        print(line[7])
 
     connection.close()
 
 list()
+
+"""First of all we imported the module for sqlite, which contains data packets like special functions etc. in order to interact with the sqlite database and execute different commands on that.
+Then we defined a funtion which will list some sort of data from database. At first, we established a connection with sqlite chinook database in sqlite browser. Afterwards, with connection.execute
+we tell the browser that he has to do something for us. With (select...) we tell him the range of data he has to permit access. With the for loop we will list every line in a certain column."""
+#.cursor(): It's an object used to execute SQL statements and data set fetch operations.
---
+++ sqliteDemo2.py
@@ -0,0 +1,72 @@
+import sqlite3
+import pandas as pd # pip install pandas   #module that provides functions to do data analysis
+
+# Create a database
+connection = sqlite3.connect('student.db')   #connects to database file and creates it automatically
+# connection = sqlite3.connect(':memory:')   #connects and creates database temporarily that exists on ram
+
+# Create a table - this what our table looks like
+sql_create_table = """
+CREATE TABLE students (
+    student_id TEXT PRIMARY KEY,
+    student_name TEXT NOT NULL,
+    gender TEXT,
+    major TEXT,
+    year_enrolelr INTEGER,
+    email TEXT UNIQUE
+)   
+"""
+
+cursor = connection.cursor()   #object used to execute sql statements
+try:
+    cursor.execute(sql_create_table)   #to create a table we first need to commit the statement
+    connection.commit()   #execute sql statement
+except Exception as e:
+    print(e)
+    connection.rollback()  #if I run into any error, do not any operation
+
+
+# Insert records
+cursor = connection.cursor()  #execute another statement - we store the output in a seperate object to continue working with it
+cursor.execute("INSERT INTO students VALUES ('A10011','student_name1','M','Computer Science',2022,'email11@gmail.com')")
+cursor.execute("INSERT INTO students VALUES ('A10013','student_name3','M','English',2018,'email13@gmail.com')")
+cursor.execute("INSERT INTO students VALUES ('A10012','student_name2','F','Biology',2021,'email12@gmail.com')")
+cursor.execute("INSERT INTO students VALUES ('A10014','student_name4','M','Accounting',2018,'email14@gmail.com')")
+cursor.execute("INSERT INTO students VALUES ('A10015','student_name5','F','Marketing',2019,'email15@gmail.com')")
+cursor.execute("INSERT INTO students VALUES ('A10016','student_name6','F','Sociology',2022,'email16@gmail.com')")
+connection.commit()             #we always need to commit sql statements
+
+# Query records
+sql_query = """
+SELECT * FROM students LIMIT 5
+"""
+rows = cursor.execute(sql_query)
+columns = [col[0] for col in rows.description]
+records = rows.fetchall()
+connection.close()
+print(columns)
+print(records)
+
+df = pd.DataFrame(records, columns=columns)
+print(df.to_csv('data.csv'))
+
+# Using WITH statement to auto close connections
+from contextlib import closing
+import sqlite3
+import pandas as pd # pip install pandas
+
+with closing(sqlite3.connect('demo.db')) as connection2:
+    with closing(connection.cursor()) as cursor2:
+        cursor2.execute("INSERT INTO students VALUES ('A10001','student_name1','M','Computer Science',2022,'email1@gmail.com')")
+        cursor2.execute("INSERT INTO students VALUES ('A10003','student_name3','M','English',2018,'email3@gmail.com')")
+        cursor2.execute("INSERT INTO students VALUES ('A10002','student_name2','F','Biology',2021,'email2@gmail.com')")
+        cursor2.execute("INSERT INTO students VALUES ('A10004','student_name4','M','Accounting',2018,'email4@gmail.com')")
+        cursor2.execute("INSERT INTO students VALUES ('A10005','student_name5','F','Marketing',2019,'email5@gmail.com')")
+        cursor2.execute("INSERT INTO students VALUES ('A10006','student_name6','F','Sociology',2022,'email6@gmail.com')")
+        connection.commit()
+
+        rows2 = cursor2.execute()
+        columns2 = [col[0] for col in rows2.description]
+        records2 = rows.fetchall()
+        df = pd.DataFrame(records, columns=columns)
+print(df)        
